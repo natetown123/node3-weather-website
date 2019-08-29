ab1da0aaa8873706021c5dcff649487cda0708d1
Nathan <NathanJWheeler1@gmail.com>
2019-08-28 23:52:51 -0400

    Added additional parameters from darksky

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -6,6 +6,7 @@ const url = `https://api.darksky.net/forecast/0cd53e0bc4263a75e704c623ee568d8a/$
         url,
         json: true
     }, (error, {body}) => {
+        console.log(body)
         if(error){
             callback('Request cannot be completed')
         } else if(body.error) {
@@ -15,8 +16,11 @@ const url = `https://api.darksky.net/forecast/0cd53e0bc4263a75e704c623ee568d8a/$
             callback(undefined, 
                 `The current temperature is ${body.currently.temperature},
                 ${body.currently.summary} with a
-                ${body.currently.precipProbability}% chance of rain`
+                ${body.currently.precipProbability}% chance of rain
+                The humidity is ${body.currently.humidity} and the wind speed is ${body.currently.windSpeed}`
+                
             )
+            
         }
     })
 }
